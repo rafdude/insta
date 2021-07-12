bde874eac073433be504fcf13f16905bcdcd2b2b
rafdude <79229372+rafdude@users.noreply.github.com>
2021-07-11 23:43:57 -0400

    Create insta.sh

---
+++ insta.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PATH=/home/user/.local/bin:/home/user/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+echo '==============================================================================================='
+figlet -f digital Started $(date)
+echo '==============================================================================================='
+echo 
+
+## mm/dd/yyyy and time###
+## Time in 12 hr format ###
+
+## now=$(date +'%m/%d/%Y @ %r')
+
+## Log message to syslog
+
+## echo   "Instragram script run @ {$now}" 
+
+echo "Step 8: Installing / Updating Instaloader"
+pip3 install instaloader
+pip3 install --upgrade instaloader
+
+echo
+pip3 install instaloader
+
+
+/home/user/.local/bin/instaloader -l robert.stack.50951 -p tL382xZoPoj8sB --no-metadata-json -C -F --dirname-pattern=/home/user/insta/files/{profile}  profile juli>
+
+
+echo '==============================================================================================='
+figlet -f digital Ended $(date)
+echo '==============================================================================================='
+echo 
